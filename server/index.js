67d71e539bc05bdfab2a144121e12864efbdad0e
William Chan <wchan1992@gmail.com>
2019-10-05 00:20:24 -0700

    Update PORT

--- server/index.js
+++ server/index.js
@@ -4,12 +4,9 @@ const path = require('path');
 const router = require('./routes');
 
 const app = express();
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../client/dist'));
-});
-// app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
