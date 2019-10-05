356add61f6f65fec6a628d7f08d3a2968710ce65
William Chan <wchan1992@gmail.com>
2019-10-05 00:10:34 -0700

    Update Express Route

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const router = require('./routes');
 const app = express();
 const PORT = process.env.port || 8080;
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/dist'));
+});
+// app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
